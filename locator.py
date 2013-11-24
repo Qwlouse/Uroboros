f77357b91e9f76718bbc0ee58acd0f2b709d2109
Qwlouse <klaus.greff@gmx.net>
2013-11-24 23:50:52 +0100

    added test for 3rd party module

--- locator.py
+++ locator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
+import imp
+import os
 import sys
 
 BUILTINS = set(name for name in sys.builtin_module_names)
@@ -13,4 +15,9 @@ def locate_module(module_name):
     if module_name in BUILTINS:
         return '__builtin__'
 
-    return "filename"
+    f, filename, (suffix, mode, type) = imp.find_module(module_name)
+
+    if os.path.isdir(filename):
+        filename = os.path.join(filename, '__init__.py')
+
+    return filename
--- test/test_locator.py
+++ test/test_locator.py
@@ -3,7 +3,6 @@
 from __future__ import division, print_function, unicode_literals
 import unittest
 
-
 from locator import locate_module
 
 
@@ -28,6 +27,10 @@ BUILTIN_MODULES_WITH_UNDERSCORES = ['__main__', '_ast', '_bisect', '_codecs',
                                     '_ssl', '_struct', '_symtable', '_warnings',
                                     '_weakref']
 
+THIRD_PARTY_MODULES = [
+    ("unittest", ensure_uncompiled_source(unittest.__file__)),
+]
+
 
 class LocatorTest(unittest.TestCase):
     def test_locate_builtin_modules(self):
@@ -37,3 +40,7 @@ class LocatorTest(unittest.TestCase):
     def test_locate_builtin_modules_that_start_with_underscore(self):
         for module_name in BUILTIN_MODULES_WITH_UNDERSCORES:
             self.assertEqual(locate_module(module_name), '__builtin__')
+
+    def test_locate_3rd_party_modules_returns_filename(self):
+        for modname, filename in THIRD_PARTY_MODULES:
+            self.assertEqual(locate_module(modname), filename)
