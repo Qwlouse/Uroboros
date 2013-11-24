60126d22729f76ecff2262f29360994145c97a9e
Qwlouse <klaus.greff@gmx.net>
2013-11-24 23:39:01 +0100

    extended locator builtin tests

--- locator.py
+++ locator.py
@@ -3,9 +3,14 @@
 from __future__ import division, print_function, unicode_literals
 import sys
 
+BUILTINS = set(name for name in sys.builtin_module_names)
+
+BUILTINS = BUILTINS.union(set(name.strip('_') for name in BUILTINS
+                              if not name.startswith('__')))
+
 
 def locate_module(module_name):
-    if module_name in [name.strip('_') for name in sys.builtin_module_names]:
+    if module_name in BUILTINS:
         return '__builtin__'
 
     return "filename"
--- test/test_locator.py
+++ test/test_locator.py
@@ -16,14 +16,24 @@ def ensure_uncompiled_source(filename):
 
 BUILTIN_MODULES = ['array', 'ast', 'binascii', 'bisect', 'codecs',
                    'collections', 'errno', 'fcntl', 'functools', 'gc', 'grp',
-                   'hashlib', 'imp', 'itertools', 'locale', 'main', 'marshal',
-                   'math', 'operator', 'posix', 'pwd', 'random', 'select',
-                   'signal', 'socket', 'spwd', 'sre', 'ssl', 'struct',
-                   'symtable', 'sys', 'syslog', 'thread', 'time', 'unicodedata',
-                   'warnings', 'weakref', 'xxsubtype', 'zipimport', 'zlib']
+                   'hashlib', 'imp', 'itertools', 'locale', 'marshal', 'math',
+                   'operator', 'posix', 'pwd', 'random', 'select', 'signal',
+                   'socket', 'spwd', 'sre', 'ssl', 'struct', 'symtable', 'sys',
+                   'syslog', 'thread', 'time', 'unicodedata', 'warnings',
+                   'weakref', 'xxsubtype', 'zipimport', 'zlib']
+
+BUILTIN_MODULES_WITH_UNDERSCORES = ['__main__', '_ast', '_bisect', '_codecs',
+                                    '_collections', '_functools', '_hashlib',
+                                    '_locale', '_random', '_socket', '_sre',
+                                    '_ssl', '_struct', '_symtable', '_warnings',
+                                    '_weakref']
 
 
 class LocatorTest(unittest.TestCase):
     def test_locate_builtin_modules(self):
         for module_name in BUILTIN_MODULES:
+            self.assertEqual(locate_module(module_name), '__builtin__')
+
+    def test_locate_builtin_modules_that_start_with_underscore(self):
+        for module_name in BUILTIN_MODULES_WITH_UNDERSCORES:
             self.assertEqual(locate_module(module_name), '__builtin__')
