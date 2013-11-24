8c54aed71f47f911cd7065f8fcba40864c06b253
Qwlouse <klaus.greff@gmx.net>
2013-11-24 03:35:57 +0100

    reformatting

--- extractor.py
+++ extractor.py
@@ -4,25 +4,23 @@ from __future__ import division, print_function, unicode_literals
 import ast
 
 
+#noinspection PyPep8Naming
 class ImportExtractor(ast.NodeVisitor):
     def __init__(self):
         self.imports = []
 
     def visit_Import(self, node):
-        lineno = node.lineno
-        #col_offset = node.col_offset
-        import_aliases = [(lineno, a.name, None, a.asname) for a in node.names
+        line_nr = node.lineno
+        import_aliases = [(line_nr, a.name, None, a.asname) for a in node.names
                           if a.name != '__future__']  # ignore import __future__
         self.imports.extend(import_aliases)
         super(ImportExtractor, self).generic_visit(node)
 
     def visit_ImportFrom(self, node):
-        lineno = node.lineno
+        line_nr = node.lineno
         module = '.' * node.level + (node.module or '')
         if module != '__future__':
-            # ignore import __future__
-            #col_offset = node.col_offset
-            import_aliases = [(lineno, module, a.name, a.asname)
+            import_aliases = [(line_nr, module, a.name, a.asname)
                               for a in node.names]
             self.imports.extend(import_aliases)
 
--- test/test_extractor.py
+++ test/test_extractor.py
@@ -12,13 +12,14 @@ SIMPLE_IMPORT_STATEMENTS = \
      ("'import justastring'", []),
      ("import __future__", []),
      ("import __future__ as __past__", []),
+
      ("import __future__, re", [(1, 're', None, None)]),
-     ("import re", [(1, 're', None, None)]),
-     ("import time", [(1, 'time', None, None)]),
-     ("import numpy as np", [(1, 'numpy', None, 'np')]),
-     ("import sys, os, copy", [(1, 'sys', None, None),
-                               (1, 'os', None, None),
-                               (1, 'copy', None, None)]),
+     ("import re",             [(1, 're', None, None)]),
+     ("import time",           [(1, 'time', None, None)]),
+     ("import numpy as np",    [(1, 'numpy', None, 'np')]),
+     ("import sys, os, copy",  [(1, 'sys', None, None),
+                                (1, 'os', None, None),
+                                (1, 'copy', None, None)]),
      ("import time as tme, copy as cpy", [(1, 'time', None, 'tme'),
                                           (1, 'copy', None, 'cpy')])
      ]
@@ -29,21 +30,19 @@ FROM_IMPORT_STATEMENTS = \
      ("from __future__ import division, print_function", []),
      ("from __future__ import division as div", []),
 
-     ("from copy import deepcopy", [(1, 'copy', 'deepcopy', None)]),
-     ("from copy import copy, deepcopy", [(1, 'copy', 'copy', None),
-                                          (1, 'copy', 'deepcopy', None)]),
-     ("from os import path as p", [(1, 'os', 'path', 'p')]),
+     ("from copy import deepcopy",        [(1, 'copy', 'deepcopy', None)]),
+     ("from copy import copy, deepcopy",  [(1, 'copy', 'copy', None),
+                                           (1, 'copy', 'deepcopy', None)]),
+     ("from os import path as p",         [(1, 'os', 'path', 'p')]),
 
-     ("from foo.bar import baz", [(1, 'foo.bar', 'baz', None)]),
-     ("from foo.bar import baz as zab", [(1, 'foo.bar', 'baz', 'zab')]),
+     ("from foo.bar import baz",          [(1, 'foo.bar', 'baz', None)]),
+     ("from foo.bar import baz as zab",   [(1, 'foo.bar', 'baz', 'zab')]),
 
-     ("from .stuff import math", [(1, '.stuff', 'math', None)]),
-     ("from .stuff import copy as cpy", [(1, '.stuff', 'copy', 'cpy')]),
-     ("from ..stuff import foo as bar", [(1, '..stuff', 'foo', 'bar')]),
-     ("from .. import stuff as stf", [(1, '..', 'stuff', 'stf')]),
+     ("from .stuff import math",          [(1, '.stuff', 'math', None)]),
+     ("from .stuff import copy as cpy",   [(1, '.stuff', 'copy', 'cpy')]),
+     ("from ..stuff import foo as bar",   [(1, '..stuff', 'foo', 'bar')]),
+     ("from .. import stuff as stf",      [(1, '..', 'stuff', 'stf')]),
      ("from ..foo.bar import baz as zab", [(1, '..foo.bar', 'baz', 'zab')]),
-
-
      ]
 
 
